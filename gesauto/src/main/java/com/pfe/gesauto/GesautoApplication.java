bb57164b99313c7553c0b73282bd98c7aef4cac2
fares fellah <faresfelleh2505@gmail.com>
2024-08-10 07:27:46 +0100

    test

---
+++ gesauto/src/main/java/com/pfe/gesauto/GesautoApplication.java
@@ -0,0 +1,13 @@
+package com.pfe.gesauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesautoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesautoApplication.class, args);
+	}
+
+}
---
+++ gesauto/src/test/java/com/pfe/gesauto/GesautoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfe.gesauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
